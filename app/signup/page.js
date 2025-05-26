4e7d460a85accffed2e84283e7410530dd5aae00
arunreddy7-alt <addula.arunkumarreddy7@gmail.com>
2025-05-26 17:10:23 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -51,7 +51,7 @@ export default function SignUp() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
